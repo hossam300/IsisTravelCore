97af780afb3ca11131530fce754e7f7be74745b1
ayat_elsherif <ayat_elsherif@yahoo.com>
2021-08-28 00:19:37 +0200

    continue2: editing homepage and all the user view

--- IsisTravelCore/wwwroot/js/main.js
+++ IsisTravelCore/wwwroot/js/main.js
@@ -26,67 +26,69 @@ tabLinks.forEach((link) => {
 });
 
 jQuery(window).scroll(function () {
-  if (jQuery(window).scrollTop() > 200) {
-    jQuery(".bg-light").css("background", "#fff");
+  if (jQuery(window).width() >= 992) {
+    if (jQuery(window).scrollTop() > 200) {
+      jQuery(".bg-light").css("background", "#fff");
 
-    jQuery(".bg-light").addClass("animated-nav");
-  } else {
-    jQuery(".bg-light ").css(
-      "background",
-      " linear-gradient(-90deg, #4e8226a6,#4e8226a6)"
-    );
-    jQuery(".bg-light").removeClass("animated-nav");
-  }
+      jQuery(".bg-light").addClass("animated-nav");
+    } else {
+      jQuery(".bg-light ").css(
+        "background",
+        " linear-gradient(-90deg, #4e8226a6,#4e8226a6)"
+      );
+      jQuery(".bg-light").removeClass("animated-nav");
+    }
 
-  if (jQuery(window).scrollTop() > 200) {
-    jQuery(".navbar-nav .nav-link").css("color", "#909090");
+    if (jQuery(window).scrollTop() > 200) {
+      jQuery(".navbar-nav .nav-link").css("color", "#909090");
 
-    jQuery(".navbar-nav .nav-link").addClass("animated-nav");
-  } else {
-    jQuery(".navbar-nav .nav-link ").css("color", "#fff");
-    jQuery(".navbar-nav .nav-link").removeClass("animated-nav");
-  }
+      jQuery(".navbar-nav .nav-link").addClass("animated-nav");
+    } else {
+      jQuery(".navbar-nav .nav-link ").css("color", "#fff");
+      jQuery(".navbar-nav .nav-link").removeClass("animated-nav");
+    }
 
-  if (jQuery(window).scrollTop() > 200) {
-    jQuery(".navbar-light .navbar-nav .active>.nav-link").css(
-      "color",
-      "rgb(146, 128, 85)"
-    );
+    if (jQuery(window).scrollTop() > 200) {
+      jQuery(".navbar-light .navbar-nav .active>.nav-link").css(
+        "color",
+        "rgb(146, 128, 85)"
+      );
 
-    jQuery(".navbar-light .navbar-nav .active>.nav-link").addClass(
-      "animated-nav"
-    );
-  } else {
-    jQuery(".navbar-light .navbar-nav .active>.nav-link ").css(
-      "color",
-      "#faffc6"
-    );
-    jQuery(".navbar-light .navbar-nav .active>.nav-link").removeClass(
-      "animated-nav"
-    );
-  }
+      jQuery(".navbar-light .navbar-nav .active>.nav-link").addClass(
+        "animated-nav"
+      );
+    } else {
+      jQuery(".navbar-light .navbar-nav .active>.nav-link ").css(
+        "color",
+        "#faffc6"
+      );
+      jQuery(".navbar-light .navbar-nav .active>.nav-link").removeClass(
+        "animated-nav"
+      );
+    }
 
-  if (jQuery(window).scrollTop() > 200) {
-    jQuery(".logo-navbar").css("opacity", "1");
+    if (jQuery(window).scrollTop() > 200) {
+      jQuery(".logo-navbar").css("opacity", "1");
 
-    jQuery(".logo-navbar").addClass("animated-nav");
-  } else {
-    jQuery(".logo-navbar").css("opacity", "0");
-    jQuery(".logo-navbar").removeClass("animated-nav");
-  }
+      jQuery(".logo-navbar").addClass("animated-nav");
+    } else {
+      jQuery(".logo-navbar").css("opacity", "0");
+      jQuery(".logo-navbar").removeClass("animated-nav");
+    }
 
-  if (jQuery(window).scrollTop() > 200) {
-    jQuery(".titile-logo").css("opacity", "0");
+    if (jQuery(window).scrollTop() > 200) {
+      jQuery(".titile-logo").css("opacity", "0");
 
-    jQuery(".titile-logo").addClass("animated-nav");
-  } else {
-    jQuery(".titile-logo").css("opacity", "1");
-    jQuery(".titile-logo").removeClass("animated-nav");
+      jQuery(".titile-logo").addClass("animated-nav");
+    } else {
+      jQuery(".titile-logo").css("opacity", "1");
+      jQuery(".titile-logo").removeClass("animated-nav");
+    }
   }
 });
 
-if (jQuery(window).width() < 992) {
-  jQuery(".carousel").carousel({
-    keyboard: false,
-  });
-}
+// if (jQuery(window).width() < 992) {
+//   jQuery(".carousel").carousel({
+//     keyboard: false,
+//   });
+// }
