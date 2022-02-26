843a13be607e72aab6c250fbf19710e94e791c59
Hossam Fathy <semsem.hos33@hotmail.com>
2022-02-26 22:23:51 +0200

    issue

--- IsisTravelCore/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ IsisTravelCore/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -47,7 +47,7 @@ namespace IsisTravelCore.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "RECUERDAME ?")]
             public bool RememberMe { get; set; }
         }
 
